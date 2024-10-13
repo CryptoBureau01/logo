099ab725d15ed3001363020b5de3ffb535099319
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-13 14:01:52 +0530

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to print the CRYPTOBURO logo
+print_logo() {
+    echo -e "\e[1;34m"
+    echo "§###############################################################################§"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "#                                                                               #"
+    echo "§###############################################################################§"
+    echo -e "\e[0m"
+}
+
+# Call the function to display the logo
+print_logo
