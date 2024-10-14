6e09a9c789b15203d50acda6e523da76a4e5adee
TesterBoy <152272581+CryptoBuroMaster@users.noreply.github.com>
2024-10-14 18:51:18 +0530

    Update and rename logo.md to logo.sh

---
+++ logo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "\e[1;32m  
+
+.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x
+   _____   ____  
+  / ____| |  _ \                 Fueled by Innovation
+ | |      | |_) |        Empowering Seamless Node Deployment    
+ | |      |  _ <             with Cutting-Edge Tools for
+ | |____  | |_) |                CryptoBuro Master
+  \_____| |____/ 
+-___________________________________________________________________________- 
+
+If you have issues with node setup or tool errors.
+Contact us on the CryptoBuro Telegram channel for support. 
+
+Github : https://github.com/CryptoBuroMaster 
+Telegram Channel : https://t.me/CryptoBuroOfficial 
+
+.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x
+
+\e[0m"
+
+
+
