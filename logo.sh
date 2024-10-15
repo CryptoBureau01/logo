631f320a634ad41671ad39e4d861f9823e2292a2
CB Master <152272581+CryptoBureau01@users.noreply.github.com>
2024-10-15 08:37:59 +0530

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -3,20 +3,21 @@
 echo -e "\e[1;32m  
 
 .x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x
-        _____   ____  
-       / ____| |  _ \                 Fueled by Innovation
-      | |      | |_) |        Empowering Seamless Node Deployment    
-      | |      |  _ <             with Cutting-Edge Tools for
-      | |____  | |_) |                CryptoBuro Master
-       \_____| |____/ 
+            _____   ____  
+           / ____| |  _ \                 Fueled by Innovation
+          | |      | |_) |        Empowering Seamless Node Deployment    
+          | |      |  _ <             with Cutting-Edge Tools for
+          | |____  | |_) |                Crypto Bureau Master
+           \_____| |____/ 
 -___________________________________________________________________________- 
 
-If you have issues with node setup or tool errors.
-Contact us on the CryptoBuro Telegram channel for support. 
+For node setup or tool errors, contact Crypto Bureau Master for support.
 
-Gmail : cryptoburo01@gmail.com
-Github : https://github.com/CryptoBuroMaster 
-Telegram Channel : https://t.me/CryptoBuroOfficial 
+- Gmail : cryptobureau01@gmail.com 
+- X : https://x.com/CryptoBureau01
+- Telegram : https://t.me/CryptoBureau01
+- Github : https://github.com/CryptoBureau01
+- YouTube : https://www.youtube.com/@CryptoBureau01
 
 .x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x.x0x
 
